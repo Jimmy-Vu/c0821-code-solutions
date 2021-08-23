6c25a7df15b12477c230549b7a18a9dbc0e5bbc1
Jimmy Vu <JMV3211@gmail.com>
2021-08-23 10:22:10 -0700

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+var fullName = 'Jimmy Vu';
+var isCool = true;
+var totalPets = 0;
+
+console.log('Output of fullName:', fullName);
+console.log('Output of isCool:', isCool);
+console.log('Output of totalPets:', totalPets);
+
+console.log('Data type of fullName:', typeof fullName);
+console.log('Data type of isCool:', typeof isCool);
+console.log('Data type of totalPets:', typeof totalPets);
+
+var unknown = null;
+
+console.log('Output of unknown:', unknown);
+console.log('Data type of unknown:', typeof unknown);
+
+var never;
+
+console.log('Output of never:', never);
+console.log('Data type of never:', typeof never);
