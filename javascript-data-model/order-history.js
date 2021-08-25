d3af840fded068567d0d3e6ae9d7daed03c52b66
Jimmy Vu <JMV3211@gmail.com>
2021-08-25 11:25:11 -0700

    Revised other orders to reflect the syntax set up by the fourth order. Nested item information into an array for the property itemInfo for all orders.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -3,51 +3,57 @@ var orderHistory = [
     orderDate: 'August 4, 2020',
     ifDelivered: true,
     deliveryDate: 'August 8,2020',
-    subtotal: 31.55,
     grandTotal: 34,
     recipient: 'JS Masher',
     orderNumber: '114-3941689-8772232',
     returnWindowOpen: false,
     returnWindowDate: 'Sep 7, 2020',
     reviewWritten: false,
-    itemInfo: {
-      type: 'book',
-      name: 'Javascript for impatient programmers',
-      author: 'Dr. Axel Rauschmayer'
-    }
+    itemInfo: [
+      {
+        type: 'book',
+        name: 'Javascript for impatient programmers',
+        author: 'Dr. Axel Rauschmayer',
+        subtotal: 31.55
+      }
+    ]
   },
   {
     orderDate: 'July 19, 2020',
     ifDelivered: true,
     deliveryDate: 'July 20,2020',
-    subtotal: 41.33,
     grandTotal: 44.53,
     recipient: 'JS Masher',
     orderNumber: '113-9984268-1280257',
     returnWindowOpen: false,
     returnWindowDate: 'Aug 19, 2020',
     reviewWritten: false,
-    itemInfo: {
-      type: 'book',
-      name: 'The Timeless Way of Building',
-      author: 'Christopher Alexander'
-    }
+    itemInfo: [
+      {
+        type: 'book',
+        name: 'The Timeless Way of Building',
+        author: 'Christopher Alexander',
+        subtotal: 41.33
+      }
+    ]
   },
   {
     orderDate: 'July 4, 2020',
     ifDelivered: true,
     deliveryDate: 'July 7,2020',
-    subtotal: 15.98,
     grandTotal: 17.22,
     recipient: 'JS Masher',
     orderNumber: '114-2875557-9059409',
     returnWindowOpen: false,
     returnWindowDate: 'Aug 5, 2020',
     reviewWritten: false,
-    itemInfo: {
-      type: 'electronic',
-      name: 'Gamecube Controller Adapter'
-    }
+    itemInfo: [
+      {
+        type: 'electronic',
+        name: 'Gamecube Controller Adapter',
+        subtotal: 15.98
+      }
+    ]
   },
   {
     orderDate: 'July 3, 2020',
