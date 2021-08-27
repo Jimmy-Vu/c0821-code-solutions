2af3047afa3e692c601b200b67d46f4520ef9d22
Jimmy Vu <JMV3211@gmail.com>
2021-08-27 15:47:25 -0700

    In progress

--- array-manipulation-1/src/head.js
+++ array-manipulation-1/src/head.js
@@ -1,1 +1,14 @@
 /* exported head */
+/*
+  input: array(any array)
+  output: the first output of the array
+
+  -create storage for the first element in the array
+  -access and assign the array at the index of 0 to the storage variable
+  -return the storage variable
+*/
+
+function head(array) {
+  var firstElement = array[0];
+  return firstElement;
+}
--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -1,1 +1,14 @@
 /* exported tail */
+/*
+  input: array(any javascript array)
+  output: array(new array containing all elements after the first)
+
+  -create a storage for the new array
+  -look at each element one by one
+  -if the element's index is not equal to 0, push the element into the new array
+  -return the new array
+*/
+
+function tail(array) {
+
+}
