d4c575db1294f292b6ebfea15f772c634218ecce
Jimmy Vu <JMV3211@gmail.com>
2021-08-31 15:16:41 -0700

    Removed unnecessary boolean flag.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,23 +1,16 @@
-var clickStatus = false;
 var dialogContainer = document.querySelector('.dialog-container');
 var openButton = document.querySelector('.open-button');
 var noButton = document.querySelector('.no-button');
 var overlay = document.querySelector('#overlay');
 
 function modalClicked(event) {
-  if (clickStatus === false) {
-    clickStatus = true;
-    dialogContainer.className = 'dialog-container-clicked';
-    overlay.className = 'dark-overlay';
-  }
+  dialogContainer.className = 'dialog-container-clicked';
+  overlay.className = 'dark-overlay';
 }
 
 function noClicked(event) {
-  if (clickStatus === true) {
-    clickStatus = false;
-    dialogContainer.className = 'dialog-container';
-    overlay.className = '';
-  }
+  dialogContainer.className = 'dialog-container';
+  overlay.className = '';
 }
 
 openButton.addEventListener('click', modalClicked);
