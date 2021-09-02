3bd9b1fcc16d49bcc05a636ce012f675b4f8a553
Jimmy Vu <JMV3211@gmail.com>
2021-09-02 08:02:31 -0700

    In progress.

--- object-manipulation-1/src/get-value.js
+++ object-manipulation-1/src/get-value.js
@@ -1,10 +1,20 @@
 /* exported getValue */
 /*
-  input: object(any javascript object)
-  output: key(a javascript key)
+  input: object(any javascript object) and key(a javascript key)
+  output: a value in object found at the key property
 
+  -create a storage for the return value
+  -go through each property of the object one by one
+  -once the key property is located, return the value inside the property
 */
 
-function getValue(object) {
+function getValue(object, key) {
+  var value;
 
+  for (var i in object) {
+    if (i === key) {
+      value = object[i];
+    }
+  }
+  return value;
 }
--- object-manipulation-1/src/get-values.js
+++ object-manipulation-1/src/get-values.js
@@ -1,1 +1,18 @@
 /* exported getValues */
+/*
+  input: object(any javascript object)
+  output: array(object's property values)
+
+  -create a storage for a newArray to be returned
+  -look at each property values in the object and push it to the new array
+  -return the array
+*/
+
+function getValues(object) {
+  var newArray = [];
+
+  for (var i in object) {
+    newArray.push(object[i]);
+  }
+  return newArray;
+}
