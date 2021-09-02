c6bd134b1094f12ba6e12a64f99f23f5debc2d78
Jimmy Vu <JMV3211@gmail.com>
2021-09-02 15:35:17 -0700

    Completed javascript-view-swapping exercise.

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,26 @@
+var tabContainer = document.querySelector('.tab-container');
+var tabList = document.querySelectorAll('.tab');
+var viewList = document.querySelectorAll('.view');
+
+tabContainer.addEventListener('click', handleTabContainerClick);
+
+function handleTabContainerClick(event) {
+  if (event.target.matches('.tab')) {
+    for (var i = 0; i < tabList.length; i++) {
+      if (tabList[i] !== event.target) {
+        tabList[i].className = 'tab';
+      } else {
+        tabList[i].className = 'tab active';
+      }
+    }
+    var dataView = event.target.getAttribute('data-view');
+
+    for (var x = 0; x < viewList.length; x++) {
+      if (dataView === viewList[i].getAttribute('data-view')) {
+        viewList[x].className = 'view';
+      } else {
+        viewList[x].className = 'hidden';
+      }
+    }
+  }
+}
