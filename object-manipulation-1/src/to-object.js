d57a5d8153874402bd652164a88d06670351092d
Jimmy Vu <JMV3211@gmail.com>
2021-09-02 17:48:31 -0700

    Completed object-manipulation-1

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,1 +1,14 @@
 /* exported toObject */
+/*
+  input: an array(containing two elements; a string key and any other js value)
+  output: an object with one property consisting of the passed keyValuePair
+
+  -create a storage for the return object
+  -assign the property and the value from the array to the object using indexes
+  -return the new object
+*/
+function toObject(keyValuePair) {
+  var newObject = {};
+  newObject[keyValuePair[0]] = keyValuePair[1];
+  return newObject;
+}
