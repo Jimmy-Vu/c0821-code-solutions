0075dabe6d22bbba34fb40357266448d8de9214d
Jimmy Vu <JMV3211@gmail.com>
2021-10-22 10:34:28 -0700

    Made requested changes to sgt-back-end

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -25,8 +25,7 @@ app.get('/api/grades', (req, res) => {
       const grade = result.rows;
       res.status(200).json(grade);
     }).catch(err => {
-      // eslint-disable-next-line no-console
-      console.log(err);
+      console.error(err);
       res.status(500).json({
         error: 'An unexpected error occured.'
       });
@@ -79,29 +78,29 @@ app.post('/api/grades', (req, res) => {
 });
 
 app.put('/api/grades/:gradeId', (req, res) => {
-  const input = [req.body.name, req.body.course, req.body.score];
+  const params = [req.body.name, req.body.course, req.body.score];
   const score = Number.parseInt(req.params.gradeId, 10);
   const id = Number.parseInt(req.params.gradeId, 10);
   const sql = `update "grades"
-               set "name" = '${input[0]}',
-                   "course" = '${input[1]}',
-                   "score" = ${input[2]}
+               set "name" = $1,
+                   "course" = $2,
+                   "score" = $3
                where "gradeId" = ${id}
                returning *;
   `;
-  if (input[2] < 0 || input[2] > 100) {
+  if (params[2] < 0 || params[2] > 100) {
     res.status(400).json({
       error: 'Please use a score between 0 and 100;'
     });
     return;
   }
-  if (typeof input[0] !== 'string' || input[0].length === 0) {
+  if (typeof params[0] !== 'string' || params[0].length === 0) {
     res.status(400).json({
       error: 'Please enter a valid name.'
     });
     return;
   }
-  if (typeof input[1] !== 'string' || input[1].length === 0) {
+  if (typeof params[1] !== 'string' || params[1].length === 0) {
     res.status(400).json({
       error: 'Please enter a valid course.'
     });
@@ -113,13 +112,13 @@ app.put('/api/grades/:gradeId', (req, res) => {
     });
     return;
   }
-  db.query(sql)
+  db.query(sql, params)
     .then(result => {
       const grade = result.rows[0];
       if (!grade) {
         res.status(404).json({ error: `Your requested note with id ${id} cannot be found. Please check your request ID and try again.` });
       } else {
-        res.status(200).json({ name: input[0], course: input[1], score: input[2] });
+        res.status(200).json(result.row[0]);
       }
     }).catch(err => {
       console.error(err);
@@ -132,9 +131,10 @@ app.put('/api/grades/:gradeId', (req, res) => {
 app.delete('/api/grades/:gradeId', (req, res) => {
   const id = Number.parseInt(req.params.gradeId, 10);
   const sql = `delete from "grades"
-               where "gradeId" = ${id}
+               where "gradeId" = $1
                returning *`;
-  db.query(sql)
+  const params = [id];
+  db.query(sql, params)
     .then(result => {
       const grade = result.rowCount;
       if (grade === 0) {
